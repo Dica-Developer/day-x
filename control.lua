b800156c4b4c871a26d0913176096af619592f56
trifirew-DTW <wuqswuqs2001@gmail.com>
2017-12-30 15:50:50 -0500

    Fixed display in multiplayer game

--- control.lua
+++ control.lua
@@ -1,31 +1,33 @@
 -- control.lua
 
 script.on_event(defines.events.on_tick, function (e)
-  if not game.players[1].gui.top.dayx_flow then
-    game.players[1].gui.top.add{
-      type="flow",
-      name="dayx_flow",
-      direction="vertical",
-      style="dayx-flow-style"
-    }
-    game.players[1].gui.top.dayx_flow.add{
-      type="label",
-      name="day_label",
-      style="dayx-label-style",
-      caption="day"
-    }
-    game.players[1].gui.top.dayx_flow.add{
-      type="label",
-      name="time_label",
-      style="dayx-label-style",
-      caption="time"
-    }
-  end
+  for i=1,#game.players do
+    if not game.players[i].gui.top.dayx_flow then
+      game.players[i].gui.top.add{
+        type="flow",
+        name="dayx_flow",
+        direction="vertical",
+        style="dayx-flow-style"
+      }
+      game.players[i].gui.top.dayx_flow.add{
+        type="label",
+        name="day_label",
+        style="dayx-label-style",
+        caption="day"
+      }
+      game.players[i].gui.top.dayx_flow.add{
+        type="label",
+        name="time_label",
+        style="dayx-label-style",
+        caption="time"
+      }
+    end
 
-  time_float = game.tick / game.surfaces[1].ticks_per_day + 1.5
-  day_int = math.floor(time_float)
-  time_h = time_float % 1 * 24
-  time_m = time_h % 1 * 60
-  game.players[1].gui.top.dayx_flow.day_label.caption = {"day-x", day_int}
-  game.players[1].gui.top.dayx_flow.time_label.caption = string.format("%02d:%02d", time_h, time_m)
+    time_float = game.tick / game.surfaces[1].ticks_per_day + 1.5
+    day_int = math.floor(time_float)
+    time_h = time_float % 1 * 24
+    time_m = time_h % 1 * 60
+    game.players[i].gui.top.dayx_flow.day_label.caption = {"day-x", day_int}
+    game.players[i].gui.top.dayx_flow.time_label.caption = string.format("%02d:%02d", time_h, time_m)
+  end
 end)
